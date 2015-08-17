140e626e5cde946c02fbe84961d041ed5a7b5fb2
Lavish Aggarwal <lucky.lavish@gmail.com>
2015-08-17 21:14:46 +0530

    models updated

--- postcard/mail/models.py
+++ postcard/mail/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
-# Create your models here.
 class Mail(models.Model):
     subject = models.CharField(max_length=127)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
-        
+
+class Thread(models.Model):
+    # TODO : Sequencing of thread
+    mail = models.ForeignKey('Mail')
+    message = models.TextField(blank=False)
+    user = models.ManyToManyField('user_auth.User', through='UserThreadMap')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+class UserThreadMap(models.Model):
+    DELIVERY_CHOICES = (
+        ('CC', 'CC'),
+        ('BCC', 'BCC'),
+        ('TO', 'TO'),
+        ('FROM', 'FROM'),
+    )
+    user = models.ForeignKey('user_auth.User')
+    thread = models.ForeignKey('Thread')
+    delivery_status = models.CharField(max_length=4, choices=DELIVERY_CHOICES)
--- postcard/user_auth/admin.py
+++ postcard/user_auth/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.contrib.admin.util import flatten_fieldsets
-
 from .models import User
 
 
